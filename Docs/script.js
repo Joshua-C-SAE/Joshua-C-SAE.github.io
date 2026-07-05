3050c12348485a528c4b38336efc05cd69d7dc84
Joshua-C-SAE <1036693@student.sae.edu.au>
2026-07-05 18:05:44 +1000

    Website Documents

---
+++ Docs/script.js
@@ -0,0 +1,56 @@
+/* Creates Pop Up Window to Preview Image */
+function openPreview(imageSrc) {
+   /*Set Pop Up Window to Flex Style */
+    const modal = document.getElementById("previewModal").style.display = "flex";
+    
+    /*Set preview image to image from source */
+    const previewImg = document.getElementById("previewImage").src = imageSrc;
+};
+
+/*Closes Pop Up Window */
+function closePreview(){
+    /*Turns off the pop up window */
+    document.getElementById("previewModal").style.display = "none";
+};
+
+/* Locate the Submit Button in the Contact Form */
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+    /* Stop the page from refreshing */
+    event.preventDefault();
+
+    /* Get the Input Name */
+    let name = document.getElementById("name").value;
+    /* Get the Input Email */
+    let email = document.getElementById("email").value;
+    /* Get the Input Message */
+    let message = document.getElementById("message").value;
+
+    /* Any Fields are Empty */
+    if (name.trim() === "" || email.trim() === "" || message.trim() === "") {
+        /* Alert the User to fill all fields */
+        alert("Please fill in all fields");
+        /* Exit Function*/
+        return;
+    }
+
+    /* Compile Message */
+    let userMessage = {
+        /* Store Name */
+        name: name,
+        /* Store Email */
+        email: email,
+        /* Store Message */
+        message: message
+    };
+
+    /* Store Compiled Message Locally */
+    /* Swap for send to email at a later stage */
+    localStorage.setItem("Message", JSON.stringify(userMessage));
+
+    /* Alert the user that their message has been sent */
+    alert(`Thank you, ${name}! Your message has been sent. I'll review it and use it to shape my projects moving forward!`);
+
+    /* Print the save message to console */
+    console.log("Saved user data:", userMessage);
+  });
+
